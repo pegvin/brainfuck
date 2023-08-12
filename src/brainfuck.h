831aa9c05a96ea392d659d9bae80863652f69e3a
pegvin <75035219+pegvin@users.noreply.github.com>
2023-08-12 20:16:23 +0530

    Initial Implementation

--- src/brainfuck.h
+++ src/brainfuck.h
@@ -8,15 +8,15 @@
 #define TAPE_SIZE 30000
 
 struct BfContext {
-	uint8_t  Tape[TAPE_SIZE];
-	uint8_t* Pointer;
+	uint8_t Tape[TAPE_SIZE];
+	int64_t Pointer;
 };
 
 // Initializes a Brainfuck Context
 int Bf_InitializeContext(struct BfContext* ctx);
 
 // Executes Brainfuck code "str" of length "len" in "ctx" Context
-int Bf_Execute(struct BfContext* ctx, const char* const str, size_t len);
+int Bf_Execute(struct BfContext* ctx, const char* const str, int64_t len);
 
 // returns a Human readable string for corresponding error code returned by Bf_Execute
 const char* const Bf_StrError(int err);
