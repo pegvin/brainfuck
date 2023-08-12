bef6d7071f9fa110113b294089c3c2310a916cf0
pegvin <75035219+pegvin@users.noreply.github.com>
2023-08-12 16:13:56 +0530

    Setup basic stuff

---
+++ src/brainfuck.h
@@ -0,0 +1,25 @@
+#ifndef BRAINFUCK_BRAINFUCK_H_INCLUDED_
+#define BRAINFUCK_BRAINFUCK_H_INCLUDED_ 1
+#pragma once
+
+#include <stdint.h>
+#include <stdlib.h>
+
+#define TAPE_SIZE 30000
+
+struct BfContext {
+	uint8_t  Tape[TAPE_SIZE];
+	uint8_t* Pointer;
+};
+
+// Initializes a Brainfuck Context
+int Bf_InitializeContext(struct BfContext* ctx);
+
+// Executes Brainfuck code "str" of length "len" in "ctx" Context
+int Bf_Execute(struct BfContext* ctx, const char* const str, size_t len);
+
+// returns a Human readable string for corresponding error code returned by Bf_Execute
+const char* const Bf_StrError(int err);
+
+#endif // BRAINFUCK_BRAINFUCK_H_INCLUDED_
+
--- src/main.c
+++ src/main.c
@@ -1,4 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "brainfuck.h"
+
 int main(void) {
+	struct BfContext ctx;
+
+	if (Bf_InitializeContext(&ctx) != 0) {
+		printf("Error: Initialize_BfContext(...) returned Non-Zero value\n");
+		return 1;
+	}
+
+	const char str[] = "+[>[<-[]>+[>+++>[+++++++++++>][>]-[<]>-]]++++++++++<]>>>>>>----.<<+++.<-..+++.<-.>>>.<<.+++.------.>-.<<+.<.";
+	size_t len = strlen(str);
+
+	int Result = Bf_Execute(&ctx, str, len);
+	if (Result != 0) {
+		printf("Error: Bf_Execute(...) %s\n", Bf_StrError(Result));
+		return 1;
+	}
+
 	return 0;
 }
 
