f908209bef57fd77d8d223260c71ad0e3466512a
Aditya <75035219+pegvin@users.noreply.github.com>
2024-02-17 02:22:50 +0530

    Initial Implementation

--- src/brainfuck.c
+++ src/brainfuck.c
@@ -1,11 +1,13 @@
 #include <stdlib.h>
 #include <strings.h>
 #include <stdio.h>
+#include <unistd.h>
 
 #include "brainfuck.h"
 
 enum Bf_Errors {
 	Bf_TAPE_OUT_OF_BOUND_ERROR = 1,
+	Bf_UNMATCHED_SQUARE_BRACKET,
 	Bf_UNKNOWN_ERROR,
 };
 
@@ -13,12 +15,80 @@ int Bf_InitializeContext(struct BfContext* ctx) {
 	if (ctx == NULL) return 1;
 
 	bzero(ctx->Tape, sizeof(ctx->Tape));
-	ctx->Pointer = &ctx->Tape[0];
+	ctx->Pointer = 0;
 
 	return 0;
 }
 
-int Bf_Execute(struct BfContext* ctx, const char* const str, size_t len) {
+int Bf_Execute(struct BfContext* ctx, const char* const str, int64_t len) {
+	if (ctx == NULL || str == NULL)
+		return Bf_UNKNOWN_ERROR;
+
+	if (len < 1) return 0;
+
+	int64_t Position = 0;
+	while (Position < len && str[Position] != '\0') {
+		char c = str[Position];
+		switch (c) {
+			case '>': ctx->Pointer++; break;
+			case '<': ctx->Pointer--; break;
+			case '+': ctx->Tape[ctx->Pointer]++; break;
+			case '-': ctx->Tape[ctx->Pointer]--; break;
+			case '.': write(STDOUT_FILENO, &ctx->Tape[ctx->Pointer], 1); break;
+			case ',': read(STDIN_FILENO, &ctx->Tape[ctx->Pointer], 1); break;
+			case '[': {
+				if (ctx->Tape[ctx->Pointer] == 0) {
+					int64_t NxtClosingBracket = Position + 1;
+					uint32_t BracketDepth = 0;
+					while (1) {
+						if (NxtClosingBracket < len) break;
+						else if (str[NxtClosingBracket] == '\0') break;
+						else if (str[NxtClosingBracket] == '[') BracketDepth++;
+						else if (str[NxtClosingBracket] == ']') {
+							if (BracketDepth == 0) break;
+							else BracketDepth--;
+						}
+
+						NxtClosingBracket++;
+					}
+					if (BracketDepth != 0) return Bf_UNMATCHED_SQUARE_BRACKET;
+					if (NxtClosingBracket < len && str[NxtClosingBracket] == ']') {
+						Position = NxtClosingBracket;
+					}
+				}
+				break;
+			}
+			case ']': {
+				if (ctx->Tape[ctx->Pointer] != 0) {
+					int64_t PrvOpeningBracket = Position - 1;
+					uint32_t BracketDepth = 0;
+					while (1) {
+						if (PrvOpeningBracket <= 0) break;
+						else if (str[PrvOpeningBracket] == ']') BracketDepth++;
+						else if (str[PrvOpeningBracket] == '[') {
+							if (BracketDepth == 0) break;
+							else BracketDepth--;
+						}
+
+						PrvOpeningBracket--;
+					}
+					if (BracketDepth != 0) return Bf_UNMATCHED_SQUARE_BRACKET;
+					if (PrvOpeningBracket > -1 && str[PrvOpeningBracket] == '[') {
+						Position = PrvOpeningBracket;
+					}
+				}
+				break;
+			}
+
+			default:
+				break;
+		}
+		if (ctx->Pointer < 0 || ctx->Pointer > sizeof(ctx->Tape)) {
+			return Bf_TAPE_OUT_OF_BOUND_ERROR;
+		}
+
+		Position++;
+	}
 
 	return 0;
 }
--- src/main.c
+++ src/main.c
@@ -1,10 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <strings.h>
+#include <errno.h>
 
 #include "brainfuck.h"
 
-int main(void) {
+int main(int argc, char** argv) {
+	if (argc < 2) {
+		printf("Usage: %s <filepath>\n", argv[0]);
+		return 1;
+	}
+
+	FILE* f = fopen(argv[1], "r");
+	if (f == NULL) {
+		printf("Error: fopen(...) %s\n", strerror(errno));
+		return 1;
+	}
+	fseek(f, 0, SEEK_END);
+	size_t len = ftell(f) + 1;
+	fseek(f, 0, SEEK_SET);
+
+	char* str = malloc(len);
+	bzero(str, len);
+	if (str == NULL) {
+		printf("Error: malloc(...) %s\n", strerror(errno));
+		return 1;
+	}
+	if (fread(str, 1, len - 1, f) != len - 1) {
+		printf("Error: fread(...) %s\n", strerror(errno));
+		return 1;
+	}
+	fclose(f);
+
 	struct BfContext ctx;
 
 	if (Bf_InitializeContext(&ctx) != 0) {
@@ -12,15 +40,13 @@ int main(void) {
 		return 1;
 	}
 
-	const char str[] = "+[>[<-[]>+[>+++>[+++++++++++>][>]-[<]>-]]++++++++++<]>>>>>>----.<<+++.<-..+++.<-.>>>.<<.+++.------.>-.<<+.<.";
-	size_t len = strlen(str);
-
 	int Result = Bf_Execute(&ctx, str, len);
 	if (Result != 0) {
 		printf("Error: Bf_Execute(...) %s\n", Bf_StrError(Result));
 		return 1;
 	}
 
+	free(str);
 	return 0;
 }
 
